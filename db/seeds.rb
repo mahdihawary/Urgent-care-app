d034245da915ad51858f5a5158b4958d4336da41
mahdihawary <Mahdihawary@gmail.com>
2020-09-10 16:46:47 -0400

    10th commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 require 'rest-client'
-Patient.destroy_all
-Specialist.destroy_all
-Symptom.destroy_all
+require 'json'
+require 'pry'
+
+# Patient.destroy_all
+# Specialist.destroy_all
+# Symptom.destroy_all
 
 
-nimr = Patient.create(name: 'nimr', password: "shrimp")
-hulk = Patient.create(name: "banner", password: "gamma")
-thor = Patient.create(name: "thor", password: "asgard")
-black_panther = Patient.create(name: "tchalla", password: "wakanda")
-black_widow = Patient.create(name:"natasha", password: "hydra")
+# nimr = Patient.create(name: 'nimr', password: "shrimp")
+# hulk = Patient.create(name: "banner", password: "gamma")
+# thor = Patient.create(name: "thor", password: "asgard")
+# black_panther = Patient.create(name: "tchalla", password: "wakanda")
+# black_widow = Patient.create(name:"natasha", password: "hydra")
 
 
 # Referral_symptoms = [{"headache": "Neurologist"}, {"migraine": "Neurologist"},{"chest pain": "Cardiologist"}, 
@@ -16,17 +19,18 @@ black_widow = Patient.create(name:"natasha", password: "hydra")
 #      {"sprain": "Orthopedic"}, {"rash": "Dermatologist"}, 
 #     {"stomach pain": "Gastroenterologist"}, {"eye pain": "Opthamologist"}]
 
-    neurologist = Specialist.create(specialist: "neurologist")
-    cardiologist = Specialist.create(specialist: "cardiologist")
-    pulmonologist = Specialist.create(specialist: "pulmonologist")
-    orthopedic = Specialist.create(specialist: "orthopedic")
-    dermatologist = Specialist.create(specialist: "dermatologist")
-    gastroenterologist = Specialist.create(specialist: "gastroenterologist")
-    opthamologist = Specialist.create(specialist: "opthamologist")
-    faucci = Specialist.create(specialist: "faucci")
+    # neurologist = Specialist.create(specialist: "neurologist")
+    # cardiologist = Specialist.create(specialist: "cardiologist")
+    # pulmonologist = Specialist.create(specialist: "pulmonologist")
+    # orthopedic = Specialist.create(specialist: "orthopedic")
+    # dermatologist = Specialist.create(specialist: "dermatologist")
+    # gastroenterologist = Specialist.create(specialist: "gastroenterologist")
+    # opthamologist = Specialist.create(specialist: "opthamologist")
+    # faucci = Specialist.create(specialist: "faucci")
 
-    headache = Symptom.create(symptom: "headache", date: "9/9/2020")
-
+    # headache = Symptom.create(symptom: "headache", date: "9/9/2020")
     
+    test_locations = RestClient.get("https://covid-19-testing.github.io/locations/:state/complete.json")
+    binding.pry
 
     
